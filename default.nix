6c6e6ee3ff2dbb9b9c2336fd3f405258ff20fa47
Marcos Magueta <maguetamarcos@gmail.com>
2023-03-10 19:06:33 -0300

    Add basic nix infrastructure (#2)
    * nix scaffolding
    
    * Add custom settings for nwm
    
    ---------
    
    Co-authored-by: EduardoLR10 <dudulr10@gmail.com>

---
+++ default.nix
@@ -0,0 +1,31 @@
+{ lib
+, llvmPackages_11
+, cmake
+, spdlog
+, abseil-cpp
+, xorg }:
+
+llvmPackages_11.stdenv.mkDerivation rec {
+  pname = "nwm";
+  version = "0.1.0";
+  
+  src = ./.;
+
+  nativeBuildInputs = [ cmake ];
+  buildInputs = [ spdlog abseil-cpp xorg.libX11.dev ];
+
+  cmakeFlags = [
+    "-DENABLE_TESTING=OFF"
+    "-DENABLE_INSTALL=ON"
+  ];
+
+  meta = with lib; {
+    homepage = "https://github.com/nixvital/nix-based-cpp-starterkit";
+    description = ''
+      A template for Nix based C++ project setup.";
+    '';
+    licencse = licenses.mit;
+    platforms = with platforms; linux ++ darwin;
+    maintainers = [ maintainers.breakds ];    
+  };
+}
